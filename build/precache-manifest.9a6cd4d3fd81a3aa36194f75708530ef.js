2f98a65496182563b820453e6f2591f5f4ce49d1
guruias123 <74144270+guruias123@users.noreply.github.com>
2023-01-24 21:58:42 +0530

    Add files via upload

---
+++ build/precache-manifest.9a6cd4d3fd81a3aa36194f75708530ef.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d97c02dd199a9bfe1779ae5bff9ed86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26132923f78b214caf19",
+    "url": "/static/css/main.5a261316.chunk.css"
+  },
+  {
+    "revision": "f1000301b3536da29da6",
+    "url": "/static/js/2.ebf5e841.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ebf5e841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26132923f78b214caf19",
+    "url": "/static/js/main.a495f011.chunk.js"
+  },
+  {
+    "revision": "ead25f5a7c724ecd1c6e",
+    "url": "/static/js/runtime-main.a1989a45.js"
+  },
+  {
+    "revision": "62d4d7d369292a9bf23762465ec6d704",
+    "url": "/static/media/Proxima Nova Bold.62d4d7d3.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/static/media/ProximaNova-Regular.410504d4.otf"
+  },
+  {
+    "revision": "2ee48cd5bbfe8b31017063ad8a2521ba",
+    "url": "/static/media/add-proposal.2ee48cd5.svg"
+  },
+  {
+    "revision": "27879c2f5443a53d294e3086720a60a0",
+    "url": "/static/media/avatar.27879c2f.png"
+  },
+  {
+    "revision": "af740ee5c4bc5d9b78471116ab47106f",
+    "url": "/static/media/bell.af740ee5.svg"
+  },
+  {
+    "revision": "f2ba0444836a9fce9b8a17c5d034a0ec",
+    "url": "/static/media/eye.f2ba0444.svg"
+  },
+  {
+    "revision": "7fe25ee8fa05c6304b3429a0f641b303",
+    "url": "/static/media/home.7fe25ee8.svg"
+  },
+  {
+    "revision": "953d939e847bb2b857abed589568cc20",
+    "url": "/static/media/like.953d939e.svg"
+  },
+  {
+    "revision": "147d3f56ee32d5f93faaeb67e39c6d0c",
+    "url": "/static/media/logo.147d3f56.png"
+  },
+  {
+    "revision": "16422e47169435173099e959520a94a1",
+    "url": "/static/media/no-eye.16422e47.svg"
+  },
+  {
+    "revision": "aeeeaae459caa29093aa09b877129dd1",
+    "url": "/static/media/numio-qr.aeeeaae4.png"
+  },
+  {
+    "revision": "899b4dd65d347e9b5068a26a749add09",
+    "url": "/static/media/pen-n-paper.899b4dd6.svg"
+  },
+  {
+    "revision": "09d7d0ce447ade31ab3076152665bbe8",
+    "url": "/static/media/reward.09d7d0ce.svg"
+  },
+  {
+    "revision": "4c1f3170b13040aa8b8793dbc9311461",
+    "url": "/static/media/users.4c1f3170.svg"
+  }
+]);
